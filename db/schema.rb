f92cae2173ebd1b1b9463f0c2e3863006d67f3c3
aberu <aberuking19@yahoo.co.jp>
2021-08-07 13:48:02 +0000

    for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2021_08_05_141857) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "status"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
